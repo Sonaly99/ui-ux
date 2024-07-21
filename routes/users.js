4223d0b56ae78fff7a10a011addd15daecdad4cb
Sonaly99 <93465955+Sonaly99@users.noreply.github.com>
2024-07-21 07:21:56 +0545

    Add files via upload

---
+++ routes/users.js
@@ -0,0 +1,22 @@
+const req = require('express/lib/request');
+const mongoose = require('mongoose');
+const plm = require("passport-local-mongoose")
+mongoose.connect("mongodb://127.0.0.1:27017/pin");
+
+const userSchema =  mongoose.Schema({
+  username: String,
+  email: String,
+  password: String,
+  profileImage: String,
+  contact: Number,
+  boards:{
+    type: Array,
+    default: []
+  },
+
+
+});
+
+userSchema.plugin(plm);
+
+module.exports = mongoose.model("user",userSchema);
